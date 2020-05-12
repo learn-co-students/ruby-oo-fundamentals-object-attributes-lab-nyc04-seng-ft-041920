76f40e1c0fee7651f58001960b517ecacd0c765c
JonTDean <JonathanTomDean@Gmail.com>
2020-05-12 12:20:43 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+
+    # name setter
+    def name=(name)
+        @name = name
+    end
+
+    # name getter
+    def name
+        @name
+    end
+
+    # job setter
+    def job=(job)
+        @job = job
+    end
+
+    # job getter
+    def job
+        @job
+    end
+end
