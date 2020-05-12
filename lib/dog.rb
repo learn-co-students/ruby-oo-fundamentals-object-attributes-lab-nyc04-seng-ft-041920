b81245f502b149444116d89dd5a5188346811444
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-05-12 16:10:10 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,20 @@
 class Dog
 
-    def initialize(name, breed)
-        @name = name
-    end
-
     def name #getter method
         @name
     end
 
-
-    def name=(new_name) #setter method
-        @name = new_name
+    def name=(name) #setter method
+        @name = name
     end
 
+    def breed
+        @breed
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
 end
 
 fido = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,17 @@
 class Person
-    def initialize(name, job)
-        
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
     end
 end
